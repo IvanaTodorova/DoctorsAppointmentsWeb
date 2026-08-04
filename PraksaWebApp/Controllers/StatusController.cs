037473bf720ec5168aaa714483b4114e1815c944
IvanaTodorova <ivana.todorova27@gmail.com>
2026-08-04 14:56:26 +0200

    popravka na kod za Status

--- PraksaWebApp/Controllers/StatusController.cs
+++ PraksaWebApp/Controllers/StatusController.cs
@@ -23,28 +23,29 @@ namespace PraksaWebApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Save(Status status)
         {
+            Console.WriteLine("ID: " + status.id);
+            Console.WriteLine("NAME: " + status.status_name);
+
             if (status.id == 0)
-            { // POST - додавање нов статус
-                await _httpClient.PostAsJsonAsync(
+            {
+                var response = await _httpClient.PostAsJsonAsync(
                     "https://localhost:7081/api/Status",
                     status
-                    );
-            } else
-            { // PUT - изменување постоечки статус
-                await _httpClient.PutAsJsonAsync(
+                );
+
+                Console.WriteLine("POST STATUS: " + response.StatusCode);
+            }
+            else
+            {
+                var response = await _httpClient.PutAsJsonAsync(
                     $"https://localhost:7081/api/Status?id={status.id}",
                     status
-                    );
-            }
-            return RedirectToAction("Index"); 
-        }
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id)
-        { 
-            await _httpClient.DeleteAsync(
-                $"https://localhost:7081/api/Status?id={id}"
                 );
-            return RedirectToAction("Index"); 
+
+                Console.WriteLine("PUT STATUS: " + response.StatusCode);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
